fd97ab795c02d94e94a5498614209fa90ee8ed24
unknown <cabila244@gmail.com>
2023-09-27 13:22:54 +0700

    first commit

---
+++ hello.java
@@ -0,0 +1,7 @@
+ public class hello{
+   public static void main (String[] args){
+System.out.println ("Nama : cabila");
+System.out.println ("NIM  : 607062330046");
+System.out.println ("kelas: D3IF-47-03)");
+   }
+ }
